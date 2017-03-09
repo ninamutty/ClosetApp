f8e6ccf7eb5aacebed0411b3643f00b84108c4a5
Nina Mutty <ninamutty@gmail.com>
2017-03-08 21:10:26 -0800

    base files

---
+++ ClosetApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClosetApp
+//
+//  Created by Nina Mutty on 3/8/17.
+//  Copyright © 2017 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
